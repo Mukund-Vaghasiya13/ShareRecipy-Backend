40cf827c43faca7a6febb2d3d64e7062a11d94c2
Mukund-Vaghasiya13 <mukundvaghasiya138@gmail.com>
2024-11-06 09:21:23 +0530

    UserList

--- src/Controllers/HomeScreen.js
+++ src/Controllers/HomeScreen.js
@@ -26,4 +26,31 @@ const listRecipes = asyncHandler(async (req, res) => {
   return res.status(200).json(recipes);
 });
 
-export { listRecipes };
+const listRecipesOfParticularUser = asyncHandler(async (req, res) => {
+  const user = req.user;
+
+  if (!user) {
+    throw new CustomError("", "Login Token Expire", 401);
+  }
+
+  const queryParam = req.query;
+
+  if (!queryParam.page) {
+    throw new CustomError("", "Specify the page Count!", 500);
+  }
+
+  const page = parseInt(queryParam.page);
+  const limit = 10;
+  const skip = (page - 1) * limit;
+
+  const recipes = await RecipesModle.find({ userId: user._id })
+    .skip(skip)
+    .limit(limit);
+
+  if (!recipes) {
+    throw new CustomError("", "Unable To fetch Recipy", 500);
+  }
+  return res.status(200).json(recipes);
+});
+
+export { listRecipes, listRecipesOfParticularUser };
--- src/Routes/RecipesRoute.js
+++ src/Routes/RecipesRoute.js
@@ -5,11 +5,15 @@ import { verifyUser } from "../Middlewares/CheckUserIsValidOrNot.js";
 const router = Router();
 
 import { createRecipes } from "../Controllers/Recipes.js";
-import { listRecipes } from "../Controllers/HomeScreen.js";
+import {
+  listRecipes,
+  listRecipesOfParticularUser,
+} from "../Controllers/HomeScreen.js";
 router
   .route("/createRecipes")
   .post(verifyUser, upload.single("RecipesImage"), createRecipes);
 
 router.route("/list").get(verifyUser, listRecipes);
+router.route("/list/user").get(verifyUser, listRecipesOfParticularUser);
 
 export const RecipeRoute = router;
